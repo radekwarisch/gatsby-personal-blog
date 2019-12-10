8f077788993cf614cfbc7a4e426142f8471bae4b
Radek Warisch <radekwarisch@Radeks-MacBook-Pro.local>
2019-12-10 08:17:13 +0100

    Add s3 config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,31 +1,49 @@
 const siteConfig = require('./site-config');
+const targetAddress = new URL(
+  process.env.TARGET_ADDRESS || 'https://rwarisch.com'
+);
 
 module.exports = {
   siteMetadata: {
     ...siteConfig,
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-sitemap`,
-    `gatsby-plugin-offline`,
-    `gatsby-transformer-json`,
-    `gatsby-transformer-remark`,
-    `gatsby-plugin-eslint`,
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
+    'gatsby-plugin-offline',
+    'gatsby-transformer-json',
+    'gatsby-transformer-remark',
+    'gatsby-plugin-eslint',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `content`,
+        name: 'content',
         path: `${__dirname}/content`,
       },
     },
-    `gatsby-plugin-sharp`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-webpack-size`,
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-webpack-size',
     {
-      resolve: `gatsby-plugin-react-svg`,
+      resolve: 'gatsby-plugin-react-svg',
       options: {
         rule: {
           include: /images\/.*\.svg$/,
+        },
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-s3',
+      options: {
+        bucketName:
+          process.env.TARGET_BUCKET_NAME ||
+          'radoslawwarisch-gatsby-blog-bucket',
+        region: process.env.AWS_REGION,
+        protocol: targetAddress.protocol.slice(0, -1),
+        hostname: targetAddress.hostname,
+        acl: null,
+        params: {
+          // In case you want to add any custom content types: https://github.com/jariz/gatsby-plugin-s3/blob/master/recipes/custom-content-type.md
         },
       },
     },
